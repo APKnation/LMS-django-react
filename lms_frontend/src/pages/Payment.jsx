f82022983938c24310394784a16e791f419d5bfd
apknation <atanasikafuka@gmail.com>
2026-04-17 01:59:41 +0300

    Remove normal error display, keep only SweetAlert

--- lms_frontend/src/pages/Payment.jsx
+++ lms_frontend/src/pages/Payment.jsx
@@ -325,12 +325,6 @@ const Payment = () => {
                   </div>
                 </div>
                 
-                {error && (
-                  <div className="mt-4 bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded-lg">
-                    {error}
-                  </div>
-                )}
-                
                 {!course?.is_free && (
                   <button
                     onClick={createOrder}
